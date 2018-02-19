619a2a215870ce2244858988a76938b9df9c3551
久田直治郎 <naojirouhisada@gmail.com>
2018-02-20 03:17:20 +0900

    Writing The UI for The Voting Screen

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
+
+export default class Voting extends React.Component{
+  
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      /* stateless componentsにはref属性は付加できないため親に付与使用する場合は、childNodes等を使用*/
+      <div ref="winner">
+        {this.props.winner ?
+          <Winner winner={this.props.winner} />:
+          <Vote {...this.props}  />
+        }
+      </div>
+    );
+  }
+};
