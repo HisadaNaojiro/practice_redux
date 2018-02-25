f0f14dc47c710ad60453217f465272218a2c7af0
久田直治郎 <naojirouhisada@gmail.com>
2018-02-25 09:48:23 +0900

    Introducing A Client-Side Redux Store

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter,hashHistory} from 'react-router-dom';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine, 2}
+    }
+  }
+});
 ReactDOM.render((
   <BrowserRouter history={hashHistory}>
     <App />
